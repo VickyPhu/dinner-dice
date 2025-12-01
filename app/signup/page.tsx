ea1fcfab1f4289c9e26649fa573ea13f1c152c67
VickyPhu <victoriaphu@gmail.com>
2025-12-01 16:20:41 +0100

    form to sign up, no css

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,3 +1,21 @@
-export function SignupPage() {
-	return <h1>Signup page</h1>;
+"use client";
+
+import { Container } from "@mui/material";
+import { signUp } from "../auth/actions";
+
+export default function SignupPage() {
+	return (
+		<Container>
+			<form action={signUp} className="signup-form">
+				<input name="email" type="email" placeholder="Email" required />
+				<input
+					name="password"
+					type="password"
+					placeholder="Password"
+					required
+				/>
+				<button type="submit">Sign up</button>
+			</form>
+		</Container>
+	);
 }
