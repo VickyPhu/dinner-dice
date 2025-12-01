4f8408c32b181e348c282a38a6e3e5f6d5101793
VickyPhu <victoriaphu@gmail.com>
2025-12-01 16:53:00 +0100

    login with email and password

--- app/auth/actions.ts
+++ app/auth/actions.ts
@@ -15,3 +15,19 @@ export async function signUp(formData: FormData) {
 	}
 	redirect("/groups");
 }
+
+export async function logIn(formData: FormData) {
+	const supabase = await createClient();
+
+	const data = {
+		email: formData.get("email") as string,
+		password: formData.get("password") as string,
+	};
+
+	const { error } = await supabase.auth.signInWithPassword(data);
+
+	if (error) {
+		redirect("/error");
+	}
+	redirect("/groups");
+}
