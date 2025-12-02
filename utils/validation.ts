8926eaedd29610dab7eaf645f6358e1842d13b1a
VickyPhu <victoriaphu@gmail.com>
2025-12-02 11:36:28 +0100

    set up validation schema with zod

---
+++ utils/validation.ts
@@ -0,0 +1,27 @@
+import z from "zod";
+
+export const signUpSchema = z.object({
+	email: z.string().email("Invalid Email address"),
+	password: z
+		.string()
+		.min(8, { message: "Password must be at least 8 characters long" })
+		.regex(/[A-Z]/, {
+			message: "Password must contain at least one uppercase letter",
+		})
+		.regex(/[a-z]/, {
+			message: "Password must contain at least one lowercase letter",
+		})
+		.regex(/[0-9]/, { message: "Password must contain at least one number" })
+		.regex(/[^A-Za-z0-9]/, {
+			message: "Password must contain at least one special character",
+		}),
+});
+
+export type signInData = z.infer<typeof signUpSchema>;
+
+export const logInSchema = z.object({
+	email: z.string().email("Invalid Email adress"),
+	password: z.string().min(1, { message: "Password is required" }),
+});
+
+export type logInData = z.infer<typeof logInSchema>;
