1c17118e34023d53d98c63d6fbc048b4d0f1968d
VickyPhu <victoriaphu@gmail.com>
2025-12-02 21:12:08 +0100

    zod schema for validation of the form when creating a group

--- utils/validation.ts
+++ utils/validation.ts
@@ -25,3 +25,28 @@ export const logInSchema = z.object({
 });
 
 export type logInData = z.infer<typeof logInSchema>;
+
+export const createGroupSchema = z
+	.object({
+		name: z.string().min(3, "Group name must be at least 3 characters long"),
+		sharing_frequency: z
+			.number()
+			.min(1, "Must share at least once per week")
+			.max(7, "Max 7 days"),
+		weekdays: z.array(z.string()).min(1, "Select at least one day"),
+		invited_emails: z.array(z.string().email()).optional(),
+	})
+	.superRefine((data, ctx) => {
+		// amount of days the user selected
+		const selectedDays = data.weekdays.length;
+
+		// should match the sharing frequency
+		if (selectedDays !== data.sharing_frequency) {
+			ctx.addIssue({
+				path: ["weekdays"],
+				code: z.ZodIssueCode.custom,
+				message: `You selected ${selectedDays} day(s), but sharing frequency is ${data.sharing_frequency}.`,
+			});
+		}
+	});
+export type createGroupData = z.infer<typeof createGroupSchema>;
