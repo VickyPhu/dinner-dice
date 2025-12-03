a94f44e127430db123e4408c8581ce170d9562f7
VickyPhu <victoriaphu@gmail.com>
2025-12-03 20:35:18 +0100

    added button to open modal to create a group

--- app/groups/page.tsx
+++ app/groups/page.tsx
@@ -1,13 +1,23 @@
-import { Container, Typography } from "@mui/material";
+"use client";
+
+import CreateGroupModal from "@/components/createGroupModal";
+import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
+import { Container, IconButton, Typography } from "@mui/material";
+import { useState } from "react";
 import { signOut } from "../auth/actions";
 
 export default function MyGroupsPage() {
+	const [open, setOpen] = useState(false);
 	return (
 		<Container>
 			<Typography variant="h1">Welcome User</Typography>
 			<form action={signOut}>
 				<button type="submit">Sign out</button>
 			</form>
+			<IconButton onClick={() => setOpen(true)} aria-label="create group">
+				<AddBoxOutlinedIcon />
+			</IconButton>
+			<CreateGroupModal open={open} onClose={() => setOpen(false)} />
 		</Container>
 	);
 }
