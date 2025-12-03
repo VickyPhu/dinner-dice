b01e0cd023e0a7df04599df4f26a51bc2163c8b8
VickyPhu <victoriaphu@gmail.com>
2025-12-03 21:40:26 +0100

    component to choose weekdays depending on selected sharing frequency added to create group modal

---
+++ components/create-group/weekdayPicker.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import {
+	Checkbox,
+	FormControlLabel,
+	FormGroup,
+	FormHelperText,
+} from "@mui/material";
+import { useFormContext } from "react-hook-form";
+
+const weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+export default function WeekdayPicker() {
+	const { watch, setValue, formState } = useFormContext();
+
+	const frequency = watch("sharing_frequency");
+	const selectedDays: string[] = watch("weekdays") || [];
+	const error = formState.errors.weekdays?.message as string | undefined;
+
+	const maxReached = selectedDays.length >= frequency;
+
+	const toggleDay = (day: string) => {
+		let updatedDays = [...selectedDays];
+
+		if (updatedDays.includes(day)) {
+			// Deselect the day
+			updatedDays = updatedDays.filter((d) => d !== day);
+		} else {
+			// Add day only if max not reached (this should not be allowed in the UI)
+			if (!maxReached) updatedDays.push(day);
+		}
+		setValue("weekdays", updatedDays, { shouldValidate: true });
+	};
+
+	return (
+		<>
+			<FormGroup row>
+				{weekdays.map((day) => {
+					const isChecked = selectedDays.includes(day);
+					const disabledCheckbox = !isChecked && maxReached;
+					return (
+						<FormControlLabel
+							key={day}
+							control={
+								<Checkbox
+									checked={isChecked}
+									onChange={() => toggleDay(day)}
+									disabled={disabledCheckbox}
+								/>
+							}
+							label={day}
+						/>
+					);
+				})}
+			</FormGroup>
+			{error && <FormHelperText error>{error}</FormHelperText>}
+		</>
+	);
+}
