e44a92d1f6007cab2822309341f3a3ac386f20c3
VickyPhu <victoriaphu@gmail.com>
2025-12-08 14:16:46 +0100

    invitation received in mail

--- app/emails/inviteEmail.tsx
+++ app/emails/inviteEmail.tsx
@@ -1,3 +1,13 @@
+import {
+	Body,
+	Container,
+	Head,
+	Heading,
+	Html,
+	Link,
+	Text,
+} from "@react-email/components";
+
 export function InviteEmail({
 	groupName,
 	inviteUrl,
@@ -6,13 +16,21 @@ export function InviteEmail({
 	inviteUrl: string;
 }) {
 	return (
-		<div>
-			<h1>You've been invited to join {groupName}</h1>
-			<p>
-				Click the link below to join the group:
-				<br />
-				<a href={inviteUrl}>{inviteUrl}</a>
-			</p>
-		</div>
+		<Html>
+			<Head />
+			<Body style={{ backgroundColor: "#ffffff", padding: "20px" }}>
+				<Container>
+					<Heading>You&apos;ve been invited to join {groupName}</Heading>
+
+					<Text>Click the link below to accept your invitation:</Text>
+
+					<Link href={inviteUrl}>{inviteUrl}</Link>
+
+					<Text>
+						If you did not expect this email, you can safely ignore it.
+					</Text>
+				</Container>
+			</Body>
+		</Html>
 	);
 }
