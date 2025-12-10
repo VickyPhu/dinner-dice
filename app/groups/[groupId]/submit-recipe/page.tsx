1397401a96b935f13595ccb1b9219cd7b910ef5c
VickyPhu <victoriaphu@gmail.com>
2025-12-10 21:02:05 +0100

    component for prep and cooking time dropdown selection

--- app/groups/[groupId]/submit-recipe/page.tsx
+++ app/groups/[groupId]/submit-recipe/page.tsx
@@ -2,15 +2,18 @@
 
 import IngredientInput from "@/components/recipe/ingredientInput";
 import StepsInput from "@/components/recipe/stepsInput";
+import TimeInput from "@/components/recipe/timeInput";
 import { Container } from "@mui/material";
 import { useState } from "react";
 
 export default function SubmitRecipePage() {
+	const [totalTime, setTotalTime] = useState("");
 	const [ingredients, setIngredients] = useState<string[]>([]);
 	const [steps, setSteps] = useState<string[]>([]);
 
 	return (
 		<Container>
+			<TimeInput value={totalTime} onChange={setTotalTime} />
 			<IngredientInput value={ingredients} onChange={setIngredients} />
 			<StepsInput value={steps} onChange={setSteps} />
 		</Container>
