f81f4ff06677c4614878fe2e1db60bd787199aca
VickyPhu <victoriaphu@gmail.com>
2025-12-10 21:06:22 +0100

    placeholder for recipe title

--- app/groups/[groupId]/submit-recipe/page.tsx
+++ app/groups/[groupId]/submit-recipe/page.tsx
@@ -3,16 +3,23 @@
 import IngredientInput from "@/components/recipe/ingredientInput";
 import StepsInput from "@/components/recipe/stepsInput";
 import TimeInput from "@/components/recipe/timeInput";
-import { Container } from "@mui/material";
+import { Container, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 
 export default function SubmitRecipePage() {
+	const [title, setTitle] = useState("");
 	const [totalTime, setTotalTime] = useState("");
 	const [ingredients, setIngredients] = useState<string[]>([]);
 	const [steps, setSteps] = useState<string[]>([]);
 
 	return (
 		<Container>
+			<Typography variant="h6">Recipe title</Typography>
+			<TextField
+				fullWidth
+				value={title}
+				onChange={(e) => setTitle(e.target.value)}
+			/>
 			<TimeInput value={totalTime} onChange={setTotalTime} />
 			<IngredientInput value={ingredients} onChange={setIngredients} />
 			<StepsInput value={steps} onChange={setSteps} />
