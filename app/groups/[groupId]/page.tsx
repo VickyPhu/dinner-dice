1f89135168e40844d2e4fd4a890e1e770e9a48bb
VickyPhu <victoriaphu@gmail.com>
2025-12-12 14:37:35 +0100

    created component for dashboard, added in page as server

--- app/groups/[groupId]/page.tsx
+++ app/groups/[groupId]/page.tsx
@@ -1,28 +1,10 @@
-"use client";
+import GroupDashboard from "@/components/group/groupDashboard";
+import { Box } from "@mui/material";
 
-import MenuCard from "@/components/group/menuCard";
-import { Container, Typography } from "@mui/material";
-import { useParams, useRouter } from "next/navigation";
-
-export default function GroupPage() {
-	const params = useParams();
-	const router = useRouter();
-	const groupId = params.groupId;
-
+export default function DashboardPage() {
 	return (
-		<Container>
-			<Typography variant="h1">Group: {groupId}</Typography>
-			<MenuCard onClick={() => router.push(`/groups/${groupId}/submit-recipe`)}>
-				<Typography variant="h2">Submit this week&apos;s recipe</Typography>
-			</MenuCard>
-			<MenuCard
-				onClick={() => router.push(`/groups/${groupId}/assigned-recipe`)}
-			>
-				<Typography variant="h2">See assigned recipe</Typography>
-			</MenuCard>
-			<MenuCard onClick={() => router.push(`/groups/${groupId}/recipes`)}>
-				<Typography variant="h2">All recipes</Typography>
-			</MenuCard>
-		</Container>
+		<Box>
+			<GroupDashboard />
+		</Box>
 	);
 }
