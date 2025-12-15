c6b364f5dc135d1ad3d447955ad89101d8c1e12b
VickyPhu <victoriaphu@gmail.com>
2025-12-15 14:34:58 +0100

    check that user is a member in the group to access page

--- proxy.ts
+++ proxy.ts
@@ -1,20 +1,45 @@
-import { NextRequest, NextResponse } from "next/server";
+import { createClient } from "@/utils/supabase/server"; // din SSR-client
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
-// For all the routes (route: groups) that needs protection
+// For all routes under /groups
 export const config = {
 	matcher: ["/groups/:path*"],
 };
 
-export async function proxy(req: NextRequest) {
+export default async function proxy(req: NextRequest) {
 	const url = req.nextUrl.clone();
 
-	const token = req.cookies.get("sb-access-token")?.value;
-	const userId = req.cookies.get("sb-user-id")?.value;
+	const supabase = await createClient();
 
-	// User gets redirected to landing page / login if they are not logged in
-	if (!token || !userId) {
+	const {
+		data: { user },
+		error: userError,
+	} = await supabase.auth.getUser();
+
+	if (userError || !user) {
 		url.pathname = "/";
 		return NextResponse.redirect(url);
 	}
+
+	// Get group id from url
+	const pathParts = req.nextUrl.pathname.split("/");
+	const groupId = pathParts[2];
+
+	if (groupId) {
+		// Check if user is a member in the group
+		const { data: membership, error: membershipError } = await supabase
+			.from("group_members")
+			.select("*")
+			.eq("group_id", groupId)
+			.eq("user_id", user.id)
+			.single();
+
+		if (membershipError || !membership) {
+			url.pathname = "/404";
+			return NextResponse.redirect(url);
+		}
+	}
+
 	return NextResponse.next();
 }
