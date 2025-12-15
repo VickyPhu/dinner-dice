2f79a53872aedeb48cf4c2e97568d19a9f707051
VickyPhu <victoriaphu@gmail.com>
2025-12-15 11:00:15 +0100

    update recipe function

--- app/groups/[groupId]/submit-recipe/actions.ts
+++ app/groups/[groupId]/submit-recipe/actions.ts
@@ -37,3 +37,34 @@ export async function submitRecipe(
 		throw new Error(error.message);
 	}
 }
+
+export async function updateRecipe(
+	groupId: string,
+	date: string,
+	recipe: RecipeFormProp
+) {
+	const supabase = await createClient();
+
+	const {
+		data: { user },
+	} = await supabase.auth.getUser();
+
+	if (!user) throw new Error("Not authenticated");
+
+	const { error } = await supabase
+		.from("recipes")
+		.update({
+			title: recipe.title,
+			time: recipe.time,
+			ingredients: recipe.ingredients,
+			steps: recipe.steps,
+		})
+		.eq("group_id", groupId)
+		.eq("user_id", user.id)
+		.eq("for_date", date);
+
+	if (error) {
+		console.error(error);
+		throw new Error(error.message);
+	}
+}
