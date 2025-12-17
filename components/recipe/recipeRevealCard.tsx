1711fe725809d1fea927f112ac0460256dfccf6c
VickyPhu <victoriaphu@gmail.com>
2025-12-17 10:16:36 +0100

    UI to reveal the assigned recipe

---
+++ components/recipe/recipeRevealCard.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { Recipe } from "@/hooks/useAssignedRecipes";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import {
+	Card,
+	CardContent,
+	Icon,
+	List,
+	ListItem,
+	Typography,
+} from "@mui/material";
+
+export default function RecipeRevealCard({ recipe }: { recipe: Recipe }) {
+	return (
+		<Card>
+			<CardContent>
+				<Typography variant="h2">{recipe.title}</Typography>
+				<Typography variant="subtitle1">
+					<Icon>
+						<AccessTimeIcon />
+					</Icon>
+					{recipe.time}
+				</Typography>
+				<Typography variant="h2">Ingredients</Typography>
+				<List>
+					{recipe.ingredients.map((ingredient, i) => (
+						<ListItem key={i}>{ingredient}</ListItem>
+					))}
+				</List>
+				<Typography variant="h2">Steps</Typography>
+				<List>
+					{recipe.steps.map((step, i) => (
+						<ListItem key={i}>
+							{i + 1}. {step}
+						</ListItem>
+					))}
+				</List>
+			</CardContent>
+		</Card>
+	);
+}
