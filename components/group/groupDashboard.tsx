327bee989f43f8370035e4f2f702c1f176986b2b
VickyPhu <victoriaphu@gmail.com>
2025-12-26 09:49:00 +0100

    added box to fix layout with gap and margin

--- components/group/groupDashboard.tsx
+++ components/group/groupDashboard.tsx
@@ -10,19 +10,23 @@ export default function GroupDashboard() {
 	const groupId = params.groupId;
 
 	return (
-		<Box>
+		<Box sx={{margin: "1rem 0rem 0rem 1.5rem"}}>
 			<Typography variant="h1">Group: {groupId}</Typography>
-			<MenuCard onClick={() => router.push(`/groups/${groupId}/submit-recipe`)}>
-				<Typography variant="h2">Submit this week&apos;s recipe</Typography>
-			</MenuCard>
-			<MenuCard
-				onClick={() => router.push(`/groups/${groupId}/assigned-recipe`)}
-			>
-				<Typography variant="h2">See assigned recipe</Typography>
-			</MenuCard>
-			<MenuCard onClick={() => router.push(`/groups/${groupId}/recipes`)}>
-				<Typography variant="h2">All recipes</Typography>
-			</MenuCard>
+			<Box display={"flex"} gap={5}>
+				<MenuCard
+					onClick={() => router.push(`/groups/${groupId}/submit-recipe`)}
+				>
+					<Typography variant="h2">Submit this week&apos;s recipe</Typography>
+				</MenuCard>
+				<MenuCard
+					onClick={() => router.push(`/groups/${groupId}/assigned-recipe`)}
+				>
+					<Typography variant="h2">See assigned recipe</Typography>
+				</MenuCard>
+				<MenuCard onClick={() => router.push(`/groups/${groupId}/recipes`)}>
+					<Typography variant="h2">All recipes</Typography>
+				</MenuCard>
+			</Box>
 		</Box>
 	);
 }
