38234c1d999d090383e2d7501219cfe661e90f83
VickyPhu <victoriaphu@gmail.com>
2025-12-26 11:45:04 +0100

    hook to fetch users in a group, show group members, names or number depending on mobile or desktop

--- components/group/groupDashboard.tsx
+++ components/group/groupDashboard.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { useGroupMember } from "@/hooks/useGroupMembers";
 import { useGroups } from "@/hooks/useGroups";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
 import { Box, Typography } from "@mui/material";
 import { useParams, useRouter } from "next/navigation";
 import MenuCard from "./menuCard";
@@ -8,15 +10,34 @@ import MenuCard from "./menuCard";
 export default function GroupDashboard() {
 	const params = useParams();
 	const router = useRouter();
-	const groupId = params.groupId;
+	const groupId =
+		typeof params.groupId === "string" ? params.groupId : undefined;
 	const { groups, loading } = useGroups();
 	const group = groups.find((g) => g.id === groupId);
+	const { members } = useGroupMember(groupId);
 
 	return (
 		<Box sx={{ margin: "1rem 1.5rem" }}>
 			<Typography variant="h1" mb={2}>
 				{loading ? "Loading..." : `Group: ${group?.name ?? "Unknown group"}`}
 			</Typography>
+
+			<Box display="flex" alignItems="center" gap={1}>
+				<PersonOutlineIcon />
+				<Box sx={{ display: { xs: "block", sm: "none" } }}>
+					<Typography>
+						{members.length} member{members.length !== 1 ? "s" : ""}
+					</Typography>
+				</Box>
+				<Box sx={{ display: { xs: "none", sm: "flex" }, gap: 1 }}>
+					{members.map((m) => (
+						<Typography key={m.user_id}>
+							{m.username ?? "Unknown user"}
+						</Typography>
+					))}
+				</Box>
+			</Box>
+
 			<Box
 				sx={{
 					display: "flex",
