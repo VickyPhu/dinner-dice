c307c42d5d807960b89dc23082519fc017d600d8
VickyPhu <victoriaphu@gmail.com>
2025-12-28 18:01:31 +0100

    added new surrounding box and h1

--- components/recipe/smallRecipeCard.tsx
+++ components/recipe/smallRecipeCard.tsx
@@ -19,73 +19,77 @@ export default function SmallRecipeCard({
 	groupId: string;
 }) {
 	return (
-		<Box
-			sx={{
-				margin: "1rem 1.5rem",
-				display: "grid",
-				gridTemplateColumns: {
-					xs: "1fr",
-					md: "repeat(auto-fit, minmax(300px, 1fr))",
-				},
-				gap: 2,
-			}}
-		>
-			{recipes.map((recipe) => (
-				<Link
-					href={`/groups/${groupId}/recipes/${recipe.id}`}
-					key={recipe.id}
-					sx={{ textDecoration: "none", flex: 1 }}
-				>
-					<Card
-						sx={{
-							height: "100%",
-							color: "var(--text)",
-							background: "var(--card-bg)",
-							boxShadow: "1px 2px 4px var(--card-shadow)",
-							transition: "all 0.2s ease-in-out",
-							cursor: "pointer",
-							"&:hover": {
-								background: "var(--card-hover)",
-								boxShadow: "2px 6px 4px var(--card-hover-shadow)",
-								"&:hover .arrow": {
-									transform: "translateX(2px)",
-								},
-							},
-						}}
+		<Box sx={{ margin: "1rem 1.5rem" }}>
+			<Typography variant="h1" sx={{ mb: 2 }}>
+				All recipes
+			</Typography>
+			<Box
+				sx={{
+					display: "grid",
+					gridTemplateColumns: {
+						xs: "1fr",
+						md: "repeat(auto-fit, minmax(300px, 1fr))",
+					},
+					gap: 2,
+				}}
+			>
+				{recipes.map((recipe) => (
+					<Link
+						href={`/groups/${groupId}/recipes/${recipe.id}`}
+						key={recipe.id}
+						sx={{ textDecoration: "none", flex: 1 }}
 					>
-						<CardContent
-							sx={{ display: "flex", flexDirection: "column", gap: 0.5 }}
+						<Card
+							sx={{
+								height: "100%",
+								color: "var(--text)",
+								background: "var(--card-bg)",
+								boxShadow: "1px 2px 4px var(--card-shadow)",
+								transition: "all 0.2s ease-in-out",
+								cursor: "pointer",
+								"&:hover": {
+									background: "var(--card-hover)",
+									boxShadow: "2px 6px 4px var(--card-hover-shadow)",
+									"&:hover .arrow": {
+										transform: "translateX(2px)",
+									},
+								},
+							}}
 						>
-							<Box
-								sx={{
-									display: "flex",
-									alignItems: "center",
-									justifyContent: "space-between",
-									gap: 2,
-								}}
+							<CardContent
+								sx={{ display: "flex", flexDirection: "column", gap: 0.5 }}
 							>
-								<Typography variant="h2">{recipe.title}</Typography>
-								<ArrowForwardIosIcon
-									className="arrow"
-									sx={{ fontSize: 20, transition: "transform 0.2s ease" }}
-								/>
-							</Box>
-							<Typography
-								variant="body1"
-								sx={{
-									display: "flex",
-									alignItems: "center",
-									gap: 0.5,
-								}}
-							>
-								<AccessTimeIcon sx={{ fontSize: 20 }} />
-								{recipe.time}
-							</Typography>
-							<Typography variant="body2">By: {recipe.username}</Typography>
-						</CardContent>
-					</Card>
-				</Link>
-			))}
+								<Box
+									sx={{
+										display: "flex",
+										alignItems: "center",
+										justifyContent: "space-between",
+										gap: 2,
+									}}
+								>
+									<Typography variant="h2">{recipe.title}</Typography>
+									<ArrowForwardIosIcon
+										className="arrow"
+										sx={{ fontSize: 20, transition: "transform 0.2s ease" }}
+									/>
+								</Box>
+								<Typography
+									variant="body1"
+									sx={{
+										display: "flex",
+										alignItems: "center",
+										gap: 0.5,
+									}}
+								>
+									<AccessTimeIcon sx={{ fontSize: 20 }} />
+									{recipe.time}
+								</Typography>
+								<Typography variant="body2">By: {recipe.username}</Typography>
+							</CardContent>
+						</Card>
+					</Link>
+				))}
+			</Box>
 		</Box>
 	);
 }
