b95f9cdb0b5c749329fe8438509b01dde07e7958
VickyPhu <victoriaphu@gmail.com>
2025-12-28 10:31:22 +0100

    added arrow with styling and adjusted layout

--- components/recipe/smallRecipeCard.tsx
+++ components/recipe/smallRecipeCard.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import { Box, Card, CardContent, Link, Typography } from "@mui/material";
 
 interface Recipe {
@@ -34,11 +35,27 @@ export default function SmallRecipeCard({
 							"&:hover": {
 								background: "var(--card-hover)",
 								boxShadow: "2px 6px 4px var(--card-hover-shadow)",
+								"&:hover .arrow": {
+									transform: "translateX(2px)",
+								},
 							},
 						}}
 					>
 						<CardContent>
-							<Typography variant="h2">{recipe.title}</Typography>
+							<Box
+								sx={{
+									display: "flex",
+									alignItems: "center",
+									justifyContent: "space-between",
+									gap: 2,
+								}}
+							>
+								<Typography variant="h2">{recipe.title}</Typography>
+								<ArrowForwardIosIcon
+									className="arrow"
+									sx={{ fontSize: 20, transition: "transform 0.2s ease" }}
+								/>
+							</Box>
 							<Typography
 								variant="body1"
 								sx={{
