63304b083a4f2d01412cd4f5977103332775c458
VickyPhu <victoriaphu@gmail.com>
2025-12-29 14:04:13 +0100

    styling on footer, adjusted layout so footer is always at the bottom of the page

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,15 @@ export default function Footer() {
 
 	return (
 		<Box component="footer">
-			<Typography variant="body1">
+			<Typography
+				variant="body2"
+				sx={{
+					py: 2,
+					textAlign: "center",
+					borderTop: "1px solid var(--button2-shadow)",
+					mt: "auto",
+				}}
+			>
 				&copy; {currentYear} Dinner Dice. All right reserved.
 			</Typography>
 		</Box>
